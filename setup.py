4dbd4d4134561318d83f30e4c30405b58975db5c
Andrew Gross <andrew.w.gross@gmail.com>
2021-06-03 14:59:37 -0400

    Fix CHANGELOG name and setup.py commands

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
 
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('CHANGELOG.rst') as changelog_file:
+    changelog = changelog_file.read()
+
 if __name__ == "__main__":
 
     setup(
         name="anonomatic_client",
         version="0.0.1",
-        description="anonomatic_client",
-        long_description=local_file("README.md"),
+        description="Simple Anonomatic Client",
+        long_description=readme + '\n\n' + changelog,
         author="Andrew Gross",
         author_email="andrew.w.gross@gmail.com",
         url="https://github.com/andrewgross",
